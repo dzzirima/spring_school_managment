22085ca8861023df079cb46b8e3e3c70e09abe19
David T Zirima <52695278+dzzirima@users.noreply.github.com>
2023-07-24 11:57:35 +0200

    feat: working on new entities

---
+++ src/main/java/black/DailyCodeBufferApplication.java
@@ -0,0 +1,14 @@
+package black;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailyCodeBufferApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(DailyCodeBufferApplication.class, args);
+    }
+
+}
---
+++ src/test/java/black/DailcodebufferApplicationTests.java
@@ -0,0 +1,13 @@
+package black;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailcodebufferApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
